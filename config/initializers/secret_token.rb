d0ce3725f097594526406de90204ff78df4b721e
Savelyev Andrey <savelyev.andrey@gmail.com>
2010-11-02 00:47:53 +0500

    create gavsav rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gavsav::Application.config.secret_token = 'f608df5b33f156b88801e3e793742133cc1ddee15fc6684467c80d86153bae097d67b222cc2cdfcdb41112fc7e1258a7af29c96eade8ee84a4dec821685bb869'
